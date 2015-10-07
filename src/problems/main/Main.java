45770c0a6f8eca6b8b1bb2c62ff309fb26c63884
Yahel Nachum <yahelnachum@yahoo.com>
2015-10-06 21:17:27 -0400

    Solved up to problem 13.
    One issue is implementation of BigInteger is not working in problem 13 and I dont know why. WIll investigate tomorrow.

--- src/problems/main/Main.java
+++ src/problems/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		ProblemsOneToOneHundred.solveOneToOneHundred(12);
+		ProblemsOneToOneHundred.solveOneToOneHundred(13);
 	}
 
 }
