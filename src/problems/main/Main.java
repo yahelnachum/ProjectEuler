d1162da1549007cdc552db54cd7a59cfb20794ea
Yahel Nachum <ynachum@wpi.edu>
2015-10-19 22:38:58 -0400

    Solved problem 26, takes about 2-3 seconds to run might have more efficient solution.

--- src/problems/main/Main.java
+++ src/problems/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int problemNum = 25;
+		int problemNum = 26;
 		Clock timer = new Clock();
 		ProblemsOneToOneHundred.solveOneToOneHundred(problemNum);
 		long delta = timer.delta();
